d84d7c2602d335724e7b4aa15236a14f456b1b89
Debongnie Nathan <he201699@students.ephec.be>
2019-12-08 20:47:26 +0100

    ajustement des méthodes
    Correction / modifications

--- src/model/database.java
+++ src/model/database.java
@@ -3,16 +3,81 @@ package model;
 import java.sql.*;
 
 public class database {
+	
+	//valeures temporaires d'un nouveau joueur (nécessite implémentation de la connexion via les vues)
+	String name = "Swithan";
+	String pswd = "123abc";
+	
+	static String team = "'Var Motiv=0'";
+	int teams = 0;
+	
+	int gold, damage;
+	
+
+	public static Connection connexion () throws ClassNotFoundException, SQLException {
 		
-	public void addPlayer () {
+		Class.forName("org.postgresql.Driver");
+		String url = "jdbc:postgresql://localhost:5432/Clicker";
+	    String user = "postgres";
+	    String passwd = "DBA";   
+	    Connection conn;
+		conn = DriverManager.getConnection(url,user, passwd);
+		return conn;
+	};
+	
+	public void updateDB () {
 		
 	};
 	
-	public void insertPlayer () {
+	public void insertDB () {
 		
 	};
+	
+	public static void createTeam (int teamCount, String team) {
+		try {
+			if(teamCount == 0) {
+				Statement state;
+				Connection connect = connexion();
+				state = connect.createStatement();
+				
+				long id = 0;
+			
+		    //L'objet affectedRows contient le résultat de la requête SQL
+			int affectedRows = state.executeUpdate("INSERT INTO team(teamname) VALUES ("+ team + ");", Statement.RETURN_GENERATED_KEYS);
+		    
+		    
+            // check the affected rows 
+            if (affectedRows > 0) {
+                // get the ID back
+                try (ResultSet rs = state.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        id = rs.getLong(1);
+                    }
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+		    ResultSet result = state.executeQuery("SELECT * FROM team WHERE teamname = "+ team);
+		    ResultSetMetaData resultMeta = result.getMetaData();
+		    
+		    System.out.println(id);
+		    while(result.next()){         
+		        for(int i = 1; i <= resultMeta.getColumnCount(); i++)
+		          System.out.print("\t" + result.getObject(i).toString() + "\t |");
+		            
+		        System.out.println("\n---------------------------------");
 
-	public void insertTeam () {
+		      }
+
+		      result.close();
+		      state.close();
+			};
+		} catch (SQLException | ClassNotFoundException ex){
+			ex.printStackTrace();
+		}
+	};
+	
+	public void insertPlayer () {
 		
 	};
 	
@@ -28,57 +93,53 @@ public class database {
 		
 	};
 	
-	public static void getArtefact (Connection connect) throws SQLException {
+	
+	/**
+	 * méthode permettant de retrouver l'artefact du joueur dans la DB
+	 * @param id : numéro de l'artefact
+	 */
+	public static void getArtefact (int id){
+		
 		//Création d'un objet Statement
-		Statement state = connect.createStatement();
-	    //L'objet ResultSet contient le résultat de la requête SQL
-	    ResultSet result = state.executeQuery("SELECT * FROM artefact");
-	    //On récupère les MetaData
-	    ResultSetMetaData resultMeta = result.getMetaData();
+		Statement state;
+		try {
+			Connection connect = connexion();
+			state = connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+		
+			//L'objet ResultSet contient le résultat de la requête SQL
+			ResultSet result = state.executeQuery("SELECT artefactname FROM artefact WHERE artefactid = "+ id);
+
+			//On récupère les MetaData
+			ResultSetMetaData resultMeta = result.getMetaData();
+
 	       
-	    System.out.println("\n***************************************************");
-	    //On affiche le nom des colonnes
-	    for(int i = 1; i <= resultMeta.getColumnCount(); i++)
-	    	System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
-	        
-	    	System.out.println("\n***************************************************");
-	         
-	    	while(result.next()){         
-	    		for(int i = 1; i <= resultMeta.getColumnCount(); i++) {
-	    			System.out.print("\t" + result.getObject(i).toString() + "\t |");
-	            
-	    			
+			System.out.println("\n***************************************************");
+			//On affiche le nom des colonnes
+			for(int i = 1; i <= resultMeta.getColumnCount(); i++)
+				System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
+	        	
+	    		System.out.println("\n***************************************************");
+	    		
+	    		while(result.next()){         
+	    			for(int i = 1; i <= resultMeta.getColumnCount(); i++) {
+	    				System.out.print("\t" + result.getObject(i).toString() + "\t |");
+	    				
+	    				
+	    			}
+	    			System.out.println("\n-------------------------------------------------");
 	    		}
-	    		System.out.println("\n-------------------------------------------------");
-	    	}
-	      result.close();
-	      state.close();	  
-	};
-	
-	public void updatePlayerPets () {
-		
-	};
-	
-	public void updatePlayerMonster () {
-		
-	};
+	    		result.close();
+	    		state.close();	
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		};
 
-	public void updatePlayerHero () {
-		
-	};
-
-	public static Connection connexion () throws SQLException, ClassNotFoundException {
-		Class.forName("org.postgresql.Driver");
-	    
-	    String url = "jdbc:postgresql://localhost:5432/Clicker";
-	    String user = "postgres";
-	    String passwd = "DBA";   
-	    Connection conn = DriverManager.getConnection(url,user, passwd);	
-	    return conn;
-	};
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-         getArtefact(connexion());
+         createTeam(0, team);
 	};
 
 }
