2ee8cc4d2f714fb2586ecb8c8db62b9825ab3db4
MatthFlash <he201687@students.ephec.be>
2019-12-13 11:21:45 +0100

    Update Interface Graphique
    Ajout de cadres, update des couleurs et agencement des icones

--- src/Vue/GUI.java
+++ src/Vue/GUI.java
@@ -108,43 +108,43 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		
 		JPanel imageEpeeDecor1 = new JPanel();
 		imageEpeeDecor1.setBounds(310, 150, 40, 50);
-		imageEpeeDecor1.setBackground(Color.white);
+		imageEpeeDecor1.setBackground(Color.darkGray);
 		window.add(imageEpeeDecor1);
 		
 		JPanel imageEpeeDecor2 = new JPanel();
 		imageEpeeDecor2.setBounds(20, 20, 40, 50);
-		imageEpeeDecor2.setBackground(Color.white);
+		imageEpeeDecor2.setBackground(Color.darkGray);
 		window.add(imageEpeeDecor2);
 		
 		JPanel imageEpeeDecor3 = new JPanel();
 		imageEpeeDecor3.setBounds(310, 20, 40, 50);
-		imageEpeeDecor3.setBackground(Color.white);
+		imageEpeeDecor3.setBackground(Color.darkGray);
 		window.add(imageEpeeDecor3);
 		
 		JPanel imageEpeeDecor4 = new JPanel();
 		imageEpeeDecor4.setBounds(20, 150, 40, 50);
-		imageEpeeDecor4.setBackground(Color.white);
+		imageEpeeDecor4.setBackground(Color.darkGray);
 		window.add(imageEpeeDecor4);
 		
-		buttonEpeeDecor1.setBackground(Color.cyan);
+		buttonEpeeDecor1.setBackground(Color.lightGray);
 		buttonEpeeDecor1.setFocusPainted(false);
 		buttonEpeeDecor1.setBorder(null);
 		buttonEpeeDecor1.setIcon(epeeDegats);
 		imageEpeeDecor1.add(buttonEpeeDecor1);
 		
-		buttonEpeeDecor2.setBackground(Color.cyan);
+		buttonEpeeDecor2.setBackground(Color.lightGray);
 		buttonEpeeDecor2.setFocusPainted(false);
 		buttonEpeeDecor2.setBorder(null);
 		buttonEpeeDecor2.setIcon(epeeDegats);
 		imageEpeeDecor2.add(buttonEpeeDecor2);
 		
-		buttonEpeeDecor3.setBackground(Color.cyan);
+		buttonEpeeDecor3.setBackground(Color.lightGray);
 		buttonEpeeDecor3.setFocusPainted(false);
 		buttonEpeeDecor3.setBorder(null);
 		buttonEpeeDecor3.setIcon(epeeDegats);
 		imageEpeeDecor3.add(buttonEpeeDecor3);
 	
-		buttonEpeeDecor4.setBackground(Color.cyan);
+		buttonEpeeDecor4.setBackground(Color.lightGray);
 		buttonEpeeDecor4.setFocusPainted(false);
 		buttonEpeeDecor4.setBorder(null);
 		buttonEpeeDecor4.setIcon(epeeDegats);
@@ -152,32 +152,32 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		
 		JPanel vaguePanel = new JPanel();
 		vaguePanel.setBounds(90, 60, 200, 70);
-		vaguePanel.setBackground(Color.white);
+		vaguePanel.setBackground(Color.darkGray);
 		window.add(vaguePanel);
 		
-		vague.setForeground(Color.black);
+		vague.setForeground(Color.white);
 		vague.setFont(stats);
 		vague.setText("Vague n°" + model.myMonster.getWaveNumber());
 		vaguePanel.add(vague);
 		
 		JPanel numberMonstrePanel = new JPanel();
 		numberMonstrePanel.setBounds(90, 120, 200, 70);
-		numberMonstrePanel.setBackground(Color.white);
+		numberMonstrePanel.setBackground(Color.darkGray);
 		window.add(numberMonstrePanel);
 		
-		numberMonstreLabel.setForeground(Color.black);
+		numberMonstreLabel.setForeground(Color.white);
 		numberMonstreLabel.setFont(stats);
 		numberMonstreLabel.setText("Monstre n°" + model.myMonster.getNumber());
 		numberMonstrePanel.add(numberMonstreLabel);
 		
 		JPanel vagueMonstrePanel = new JPanel();
 		vagueMonstrePanel.setBounds(10, 10, 350, 200);
-		vagueMonstrePanel.setBackground(Color.white);
+		vagueMonstrePanel.setBackground(Color.darkGray);
 		window.add(vagueMonstrePanel);
 		
 		JPanel monstrePanel = new JPanel();
-		monstrePanel.setBounds(500, 140, 200, 230);
-		monstrePanel.setBackground(Color.white);
+		monstrePanel.setBounds(500, 155, 200, 215);
+		monstrePanel.setBackground(Color.darkGray);
 		window.add(monstrePanel);
 		
 		JPanel attributePanel = new JPanel();
@@ -189,7 +189,7 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		JPanel heroPanel = new JPanel();
 		heroPanel.setLayout(new GridLayout(1,1));
 		heroPanel.setBounds(525, 390, 150, 150);
-		heroPanel.setBackground(Color.white);
+		heroPanel.setBackground(Color.lightGray);
 		window.add(heroPanel);
 		
 		buttonHero.setBackground(Color.white);
@@ -208,65 +208,65 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		monstrePanel.add(buttonMonster);
 		
 		JPanel pvMonstrePanel1 = new JPanel();
-		pvMonstrePanel1.setBounds(505,87,44,64);
+		pvMonstrePanel1.setBounds(505,101,44,50);
 		pvMonstrePanel1.setBackground(Color.white);
 		window.add(pvMonstrePanel1);
 		
 		JPanel pvMonstrePanel2 = new JPanel();
-		pvMonstrePanel2.setBounds(650,87,44,64);
+		pvMonstrePanel2.setBounds(650,101,44,50);
 		pvMonstrePanel2.setBackground(Color.white);
 		window.add(pvMonstrePanel2);
 		
 		JPanel coinUpgrade = new JPanel();
-		coinUpgrade.setBounds(910,162,30,34);
-		coinUpgrade.setBackground(Color.cyan);
+		coinUpgrade.setBounds(910,127,30,34);
+		coinUpgrade.setBackground(Color.darkGray);
 		window.add(coinUpgrade);
 		
-		upgradeGold.setBackground(Color.cyan);
+		upgradeGold.setBackground(Color.darkGray);
 		upgradeGold.setFocusPainted(false);
 		upgradeGold.setBorder(null);
 		upgradeGold.setIcon(littleGold);
 		coinUpgrade.add(upgradeGold);
 		
 		JPanel coinNewPet = new JPanel();
-		coinNewPet.setBounds(1020,162,30,34);
-		coinNewPet.setBackground(Color.cyan);
+		coinNewPet.setBounds(1020,127,30,34);
+		coinNewPet.setBackground(Color.darkGray);
 		window.add(coinNewPet);
 		
-		newPetGold.setBackground(Color.cyan);
+		newPetGold.setBackground(Color.darkGray);
 		newPetGold.setFocusPainted(false);
 		newPetGold.setBorder(null);
 		newPetGold.setIcon(littleGold);
 		coinNewPet.add(newPetGold);
 		
 		JPanel coinUpgradePets = new JPanel();
-		coinUpgradePets.setBounds(1020,271,30,34);
-		coinUpgradePets.setBackground(Color.cyan);
+		coinUpgradePets.setBounds(1020,236,30,34);
+		coinUpgradePets.setBackground(Color.darkGray);
 		window.add(coinUpgradePets);
 		
-		upgradePetsGold.setBackground(Color.cyan);
+		upgradePetsGold.setBackground(Color.darkGray);
 		upgradePetsGold.setFocusPainted(false);
 		upgradePetsGold.setBorder(null);
 		upgradePetsGold.setIcon(littleGold);
 		coinUpgradePets.add(upgradePetsGold);
 		
 		JPanel coinReset = new JPanel();
-		coinReset.setBounds(910,271,30,34);
-		coinReset.setBackground(Color.cyan);
+		coinReset.setBounds(910,236,30,34);
+		coinReset.setBackground(Color.darkGray);
 		window.add(coinReset);
 		
-		resetGold.setBackground(Color.cyan);
+		resetGold.setBackground(Color.darkGray);
 		resetGold.setFocusPainted(false);
 		resetGold.setBorder(null);
 		resetGold.setIcon(littleArtefactGold);
 		coinReset.add(resetGold);
 		
 		JPanel coinNewArtefact = new JPanel();
-		coinNewArtefact.setBounds(965,382,30,44);
-		coinNewArtefact.setBackground(Color.cyan);
+		coinNewArtefact.setBounds(970,347,30,44);
+		coinNewArtefact.setBackground(Color.darkGray);
 		window.add(coinNewArtefact);
 
-		newArtefactGold.setBackground(Color.cyan);
+		newArtefactGold.setBackground(Color.darkGray);
 		newArtefactGold.setFocusPainted(false);
 		newArtefactGold.setBorder(null);
 		newArtefactGold.setIcon(littleArtefactGold);
@@ -286,11 +286,11 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		pvMonstrePanel2.add(buttonCoeur2);
 		
 		JPanel texteTableauDesScores = new JPanel();
-		texteTableauDesScores.setBounds(10,400,350,250);
-		texteTableauDesScores.setBackground(Color.cyan);
+		texteTableauDesScores.setBounds(10,220,350,420);
+		texteTableauDesScores.setBackground(Color.darkGray);
 		window.add(texteTableauDesScores);
 		
-		tableauDesScores.setForeground(Color.black);
+		tableauDesScores.setForeground(Color.white);
 		tableauDesScores.setFont(stats);
 		tableauDesScores.setText("Meilleurs joueurs :");
 		texteTableauDesScores.add(tableauDesScores);
@@ -300,80 +300,80 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		//  BOUTONS D'AMELIORATION
 
 		JPanel imagesUpDegatsNewPixie = new JPanel();
-		imagesUpDegatsNewPixie.setBounds(900,100,230,67);
+		imagesUpDegatsNewPixie.setBounds(900,65,230,67);
 		imagesUpDegatsNewPixie.setLayout(new GridLayout(1,2));
-		imagesUpDegatsNewPixie.setBackground(Color.cyan);
+		imagesUpDegatsNewPixie.setBackground(Color.darkGray);
 		window.add(imagesUpDegatsNewPixie);
 		
 		JPanel textesUpDegatsNewPixie = new JPanel();
-		textesUpDegatsNewPixie.setBounds(900,150,230,67);
+		textesUpDegatsNewPixie.setBounds(900,115,230,67);
 		textesUpDegatsNewPixie.setLayout(new GridLayout(1,2));
-		textesUpDegatsNewPixie.setBackground(Color.cyan);
+		textesUpDegatsNewPixie.setBackground(Color.darkGray);
 		window.add(textesUpDegatsNewPixie);
 		
 		JPanel imagesResetUpPet = new JPanel();
-		imagesResetUpPet.setBounds(900,210,230,67);
+		imagesResetUpPet.setBounds(900,175,230,67);
 		imagesResetUpPet.setLayout(new GridLayout(1,2));
-		imagesResetUpPet.setBackground(Color.cyan);
+		imagesResetUpPet.setBackground(Color.darkGray);
 		window.add(imagesResetUpPet);
 		
 		JPanel textesResetUpPet = new JPanel();
-		textesResetUpPet.setBounds(900,260,230,67);
+		textesResetUpPet.setBounds(900,225,230,67);
 		textesResetUpPet.setLayout(new GridLayout(1,2));
-		textesResetUpPet.setBackground(Color.cyan);
+		textesResetUpPet.setBackground(Color.darkGray);
 		window.add(textesResetUpPet);
 		
 		JPanel imageBuyArtefact = new JPanel();
-		imageBuyArtefact.setBounds(900,320,230,67);
+		imageBuyArtefact.setBounds(900,285,230,67);
 		imageBuyArtefact.setLayout(new GridLayout(1,2));
-		imageBuyArtefact.setBackground(Color.cyan);
+		imageBuyArtefact.setBackground(Color.darkGray);
 		window.add(imageBuyArtefact);
 		
 		JPanel textesBuyArtefact = new JPanel();
-		textesBuyArtefact.setBounds(900,370,230,67);
+		textesBuyArtefact.setBounds(900,335,230,67);
 		textesBuyArtefact.setLayout(new GridLayout(1,2));
-		textesBuyArtefact.setBackground(Color.cyan);
+		textesBuyArtefact.setBackground(Color.darkGray);
 		window.add(textesBuyArtefact);
 
 		JPanel choiceClass = new JPanel();
         choiceClass.setLayout(new GridLayout(1,3));
-        choiceClass.setBounds(400,10,400,70);
-        choiceClass.setBackground(Color.white);
+        choiceClass.setBounds(400,20,400,70);
+        choiceClass.setBackground(Color.darkGray);
         window.add(choiceClass);
         
         JPanel degats = new JPanel();
         degats.setBounds(950,476,50,70);
-        degats.setBackground(Color.white);
+        degats.setBackground(Color.darkGray);
         window.add(degats);
         
         JPanel coin = new JPanel();
         coin.setBounds(950,520,50,70);
-        coin.setBackground(Color.white);
+        coin.setBackground(Color.darkGray);
         window.add(coin);
         
         JPanel artefactCoin = new JPanel();
         artefactCoin.setBounds(950,570,50,70);
-        artefactCoin.setBackground(Color.white);
+        artefactCoin.setBackground(Color.darkGray);
         window.add(artefactCoin);
         
         JPanel textesDegats = new JPanel();
         textesDegats.setBounds(900,482,300,30);
-        textesDegats.setBackground(Color.white);
+        textesDegats.setBackground(Color.darkGray);
 		window.add(textesDegats);
 		
 		JPanel textesCoin = new JPanel();
 		textesCoin.setBounds(900,540,300,30);
-		textesCoin.setBackground(Color.white);
+		textesCoin.setBackground(Color.darkGray);
 	    window.add(textesCoin);
 			
 	    JPanel textesArtefactCoin = new JPanel();
 	    textesArtefactCoin.setBounds(900,590,300,30);
-	    textesArtefactCoin.setBackground(Color.white);
+	    textesArtefactCoin.setBackground(Color.darkGray);
 		window.add(textesArtefactCoin);
         
 		ImageIcon UPIcon = new ImageIcon(game.class.getResource("/images/anim up.gif"));
 		
-		buttonUP.setBackground(Color.cyan);
+		buttonUP.setBackground(Color.darkGray);
 		buttonUP.setFocusPainted(false);
 		buttonUP.setBorderPainted(false);
 		buttonUP.setIcon(UPIcon);
@@ -382,26 +382,26 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		
 		ImageIcon newPixie = new ImageIcon(game.class.getResource("/images/anim pixie.gif"));
 		
-		buttonNewPet.setBackground(Color.cyan);
+		buttonNewPet.setBackground(Color.darkGray);
 		buttonNewPet.setFocusPainted(false);
 		buttonNewPet.setBorder(null);
 		buttonNewPet.setIcon(newPixie);
 		buttonNewPet.addActionListener(this);
 		imagesUpDegatsNewPixie.add(buttonNewPet);
 		
-		dmgUPLabel.setForeground(Color.black);
+		dmgUPLabel.setForeground(Color.white);
 		dmgUPLabel.setFont(stats);
 		dmgUPLabel.setText("        " + myGame.getUpgradeMoneyValue());
 		textesUpDegatsNewPixie.add(dmgUPLabel);
 		
-		newPetLabel.setForeground(Color.black);
+		newPetLabel.setForeground(Color.white);
 		newPetLabel.setFont(stats);
 		newPetLabel.setText("        " + pet.getPetCostBuy());
 		textesUpDegatsNewPixie.add(newPetLabel);
 		
 		ImageIcon reset = new ImageIcon(game.class.getResource("/images/anim reset.gif"));
 
-		buttonReborn.setBackground(Color.cyan);
+		buttonReborn.setBackground(Color.darkGray);
 		buttonReborn.setFocusPainted(false);
 		buttonReborn.setBorder(null);
 		buttonReborn.setIcon(reset);
@@ -410,7 +410,7 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		
 		ImageIcon upPetIcon = new ImageIcon(game.class.getResource("/images/anim upPets.gif"));
 		
-		buttonUpPets.setBackground(Color.cyan);
+		buttonUpPets.setBackground(Color.darkGray);
 		buttonUpPets.setFocusPainted(false);
 		buttonUpPets.setBorder(null);
 		buttonUpPets.setIcon(upPetIcon);
@@ -418,90 +418,106 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		imagesResetUpPet.add(buttonUpPets);
 		
 		
-		artfMoneyToGet.setForeground(Color.black);
+		artfMoneyToGet.setForeground(Color.white);
 		artfMoneyToGet.setFont(stats);
 		artfMoneyToGet.setText("        +" + (monstre.getWaveNumber() + myGame.getNbrUpgrade() / 10 + model.myPets.getPetNumber() /10 -1));
 		textesResetUpPet.add(artfMoneyToGet);
 		
-		upPetsLabel.setForeground(Color.black);
+		upPetsLabel.setForeground(Color.white);
 		upPetsLabel.setFont(stats);
 		upPetsLabel.setText("        " + pet.getPetCostUpgrade());
 		textesResetUpPet.add(upPetsLabel);
 
 		ImageIcon artfIcon = new ImageIcon(game.class.getResource("/images/anim newArtefact.gif"));
 
-		buttonArtf.setBackground(Color.cyan);
+		buttonArtf.setBackground(Color.darkGray);
 		buttonArtf.setFocusPainted(false);
 		buttonArtf.setBorder(null);
 		buttonArtf.setIcon(artfIcon);
 		buttonArtf.addActionListener(this);
 		imageBuyArtefact.add(buttonArtf);
 		
-		artfMoneyCost.setForeground(Color.black);
+		artfMoneyCost.setForeground(Color.white);
 		artfMoneyCost.setFont(stats);
-		artfMoneyCost.setText("                     " + hero.getArtefactCost() + "€");
+		artfMoneyCost.setText("                     " + hero.getArtefactCost());
 		textesBuyArtefact.add(artfMoneyCost);
 				
-		epeeButton.setBackground(Color.cyan);
+		epeeButton.setBackground(Color.darkGray);
 		epeeButton.setFocusPainted(false);
 		epeeButton.setBorder(null);
 		epeeButton.setIcon(epeeDegats);
 		degats.add(epeeButton);
 		
-		goldButton.setBackground(Color.white);
+		goldButton.setBackground(Color.darkGray);
 		goldButton.setFocusPainted(false);
 		goldButton.setBorder(null);
 		goldButton.setIcon(bigGold);
 		coin.add(goldButton);
 		
-		artefactGoldButton.setBackground(Color.white);
+		artefactGoldButton.setBackground(Color.darkGray);
 		artefactGoldButton.setFocusPainted(false);
 		artefactGoldButton.setBorder(null);
 		artefactGoldButton.setIcon(bigArtefactGold);
 		artefactCoin.add(artefactGoldButton);
 		
-		degatLabel.setForeground(Color.black);
+		degatLabel.setForeground(Color.white);
 		degatLabel.setFont(stats);
 		degatLabel.setText("Degats : " + hero.getDamage());
 		textesDegats.add(degatLabel);
 		
-		coinLabel.setForeground(Color.black);
+		coinLabel.setForeground(Color.white);
 		coinLabel.setFont(stats);
 		coinLabel.setText(" " + model.getGold());
 		textesCoin.add(coinLabel);
 		
-		artefactCoinLabel.setForeground(Color.black);
+		artefactCoinLabel.setForeground(Color.white);
 		artefactCoinLabel.setFont(stats);
 		artefactCoinLabel.setText(" " + hero.getArtefactMoney());
 		textesArtefactCoin.add(artefactCoinLabel);
 
 		
-
 		
-
-	/*	
-		artfMoneyCost.setForeground(Color.black);
-		artfMoneyCost.setFont(stats);
-		artfMoneyCost.setText(hero.getArtefactCost() + "$");
-		ensembleBoutton1.add(artfMoneyCost);
-	
-	*/	
 		// COMPTEUR
 		
 
 		
 		JPanel pointDeVie = new JPanel();
-		pointDeVie.setBounds(550,100,100,50);
+		pointDeVie.setBounds(550,101,100,50);
 		pointDeVie.setBackground(Color.white);
 		window.add(pointDeVie);
 		
-		PVLabel.setForeground(Color.black );
-		Font PVEcriture = new Font("Comic Sans MS", Font.PLAIN, 24 );
+		PVLabel.setForeground(Color.red);
+		Font PVEcriture = new Font("Comic Sans MS", Font.PLAIN, 35 );
 		PVLabel.setFont(PVEcriture);
-		PVLabel.setText("-  " + monstre.getPV() + "  -");
+		PVLabel.setText("   " + monstre.getPV() + "   ");
 		pointDeVie.add(PVLabel);
 		
+		JPanel backgroundGauche = new JPanel();
+		backgroundGauche.setBounds(0,0,373,700);
+		backgroundGauche.setBackground(Color.lightGray);
+		window.add(backgroundGauche);
 		
+		JPanel backgroundHaut2 = new JPanel();
+		backgroundHaut2.setBounds(373,10,455,80);
+		backgroundHaut2.setBackground(Color.darkGray);
+		window.add(backgroundHaut2);
+		
+		JPanel backgroundHaut1 = new JPanel();
+		backgroundHaut1.setBounds(373,0,455,100);
+		backgroundHaut1.setBackground(Color.lightGray);
+		window.add(backgroundHaut1);
+
+		JPanel backgroundMilieu = new JPanel();
+		backgroundMilieu.setBounds(373,0,455,700);
+		backgroundMilieu.setBackground(Color.white);
+		window.add(backgroundMilieu);
+		
+		JPanel backgroundDroite = new JPanel();
+		backgroundDroite.setBounds(828,0,373,700);
+		backgroundDroite.setBackground(Color.lightGray);
+		window.add(backgroundDroite);
+		
+
 		
 		
 		ImageIcon aquaIcon = new ImageIcon(game.class.getResource("/images/aqua.png"));
@@ -534,21 +550,21 @@ public class GUI extends gameVue implements Observer, ActionListener{
 		ImageIcon berserkIcon = new ImageIcon(game.class.getResource("/images/anim berzerker.gif"));
 		ImageIcon mageIcon = new ImageIcon(game.class.getResource("/images/anim mage.gif"));
 		
-		archerChoice.setBackground(Color.white);
+		archerChoice.setBackground(Color.darkGray);
 		archerChoice.setFocusPainted(false);
 		archerChoice.setBorderPainted(false);
 		archerChoice.addActionListener(this);
 		archerChoice.setIcon(archerIcon);
 		archerChoice.setEnabled(false);
 		
-		berserkChoice.setBackground(Color.white);
+		berserkChoice.setBackground(Color.darkGray);
 		berserkChoice.setFocusPainted(false);
 		berserkChoice.setBorderPainted(false);
 		berserkChoice.addActionListener(this);
 		berserkChoice.setIcon(berserkIcon);
 		berserkChoice.setEnabled(false);
 		
-		mageChoice.setBackground(Color.white);
+		mageChoice.setBackground(Color.darkGray);
 		mageChoice.setFocusPainted(false);
 		mageChoice.setBorderPainted(false);
 		mageChoice.addActionListener(this);
@@ -604,7 +620,7 @@ public class GUI extends gameVue implements Observer, ActionListener{
 
 	@Override
 	public void update(Observable o, Object arg) {
-		PVLabel.setText("-  " + model.myMonster.getPV() + "  -");
+		PVLabel.setText("   " + model.myMonster.getPV() + "   ");
 		
 		numberMonstreLabel.setText("Monstre n°" + model.myMonster.getNumber());
 		vague.setText("Vague n°" + model.myMonster.getWaveNumber());
