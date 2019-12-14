d0144593438489e46e5802cec2c0f36cfae043f4
Debongnie Nathan <he201699@students.ephec.be>
2019-12-14 12:49:08 +0100

    Reglages problemes

--- src/model/database.java
+++ src/model/database.java
@@ -104,19 +104,28 @@ public class database {
 		try {
 			Connection connect = connexion();
 			Statement state = connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			String whereTeam = " WHERE teamname = '"+team+"'";
+			String whereTeam = " WHERE team.teamname = '"+team+"'";
+			//SELECT playerid, gold, pets FROM hero
 			ResultSet resultHero = data("playerid, gold, pets", "hero","");
-			ResultSet resultPlayer = data("playerid", "player JOIN team ON team.teamid = player.teamid", whereTeam);
+			//SELECT playerid, teamname FROM player 
+			ResultSet resultPlayer = data("playerid, teamname", "player JOIN team ON team.teamid = player.teamid", whereTeam);
 			int teammoney = 0;
 			int teampets = 0;
+			int playerID;
+			int heroID;
+			resultPlayer.beforeFirst();
+			resultHero.beforeFirst();
 			while(resultPlayer.next()) {
 				resultHero.next();
-				if(resultPlayer.getObject(1).toString().equals(resultHero.getObject(1).toString())) {
+				playerID = Integer.parseInt(resultPlayer.getObject(1).toString());
+				heroID = Integer.parseInt(resultHero.getObject(1).toString());
+				if(playerID == heroID) {
 					teammoney += Integer.parseInt(resultHero.getObject(2).toString());
 					teampets += Integer.parseInt(resultHero.getObject(3).toString());
 				}
+				affectedRows = state.executeUpdate("UPDATE team SET teammoney = "+teammoney+", teampets ="+teampets+" WHERE teamname = '"+team+"'");
 			}
-			affectedRows = state.executeUpdate("UPDATE team SET teammoney = "+teammoney+", teampets ="+teampets);
+			//System.out.println(team + " : "+teammoney+" "+teampets);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -282,9 +291,147 @@ public class database {
 		return affectedRows;
 	}	
 	
-	private int connectionPlayer(String name, String password, String team) {
-		
+	private int createTeamPlayer(String name, String password, String team) {
+		try {
+			Connection connect = connexion();
+			Statement state = connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			affectedRows = insertTeam(team);
+			affectedRows += insertPlayer(name, password, team);
+			affectedRows += insertHero(name);
+			affectedRows += insertPets(name);
+			affectedRows += insertMonster(name);
+			affectedRows += updateTeam(team);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return affectedRows;
+	}
+	private int createPlayer(String name, String password, String team) {
+		try {
+			Connection connect = connexion();
+			Statement state = connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			affectedRows = insertPlayer(name, password, team);
+			affectedRows += insertHero(name);
+			affectedRows += insertPets(name);
+			affectedRows += insertMonster(name);
+			affectedRows += updateTeam(team);
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return affectedRows;
+	}
+	
+	private boolean checkTeam(String team) {
+		boolean check = false;
+		ResultSet resultTeam = data("*","team","");
+		try {
+			//si il y a des équipes dans la table
+			if(resultTeam.first()) {
+				resultTeam.beforeFirst();
+				//tant qu'il y a des équipes dans la table
+				while(resultTeam.next()) {
+					//si l'équipe en paramètre = l'équipe sélectionnée
+					if(team.equals(resultTeam.getObject(2))) {
+						check = true;
+					}
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return check;
+	}
+	private boolean checkPlayer (String name, String password) {
+		boolean check = false;
+		ResultSet resultPlayer = data("playername, playerpassword", "player", "");
+		
+		try {
+			//si il y a au moins 1 joueur
+			if(resultPlayer.first()) {
+				//tant qu'il y a des joueurs
+				resultPlayer.beforeFirst();
+				while(resultPlayer.next()) {
+					String resultName = resultPlayer.getObject(1).toString();
+					String resultPassword = resultPlayer.getObject(2).toString();
+					//Si le mot de passe est correct pour ce joueur
+					if(resultName.equals(name) && resultPassword.equals(password)) {
+						check = true;
+						System.out.println("Player ok");
+						return check;
+					}
+					else if(resultName.equals(name)) {
+						System.out.println("Password error");
+						return check;
+					}
+				}
+				System.out.println("Player not found");
+				return check;
+			}
+			System.out.println("No player found");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return check;
+	}
+	private boolean checkPlayerName (String name) {
+		boolean check = false;
+		ResultSet resultPlayer = data("playername", "player", "");
+		
+		try {
+			//si il y a au moins 1 joueur
+			//if(resultPlayer.first()) {
+				//tant qu'il y a des joueurs
+				resultPlayer.beforeFirst();
+				while(resultPlayer.next()) {
+					String resultName = resultPlayer.getObject(1).toString();
+					//Si le mot de passe est correct pour ce joueur
+					if(resultName.equals(name)) {
+						check = true;
+						return check;
+					}
+				}
+			//}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return check;
+	}
+	
+	private boolean gameConnection(String name, String password, String team) {
+		boolean connect = false;
+		boolean checkTeam = checkTeam(team);
+		System.out.println(checkTeam);
+		boolean checkPlayer = checkPlayer(name, password);
+		boolean checkPlayerName = checkPlayerName(name);
+		//si l'équipe existe
+		if(checkTeam) {
+			//si le joueur existe
+			if(checkPlayer) {
+				connect = true;
+			}
+			//si le joueur existe pas
+			else {
+				if(!checkPlayerName) {
+					//rajouter le joueur (dans toutes les tables)
+					createPlayer(name, password, team);
+					if(checkPlayer) {
+						connect = true;
+					}
+				}
+			}
+		}
+		//si l'équipe existe pas
+		else {
+			if(!checkPlayerName) {
+				//rajouter l'équipe et le joueur (dans toutes les tables)
+				createTeamPlayer(name, password, team);
+				if(checkPlayer && checkTeam) {
+					connect=true;
+				}
+			}
+		}
+		return connect;
 	}
 	
 	public ArrayList<String> classement(ArrayList<String> classement, boolean table, String ordre){
@@ -303,15 +450,20 @@ public class database {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		database db = new database(); 
-		int id = db.getId("Swithan");
-		System.out.println(id);
-		
+				
 		//player, team et password sont à insérer dans l'interface graphique
 		
-		//a garder dans l'interface graphique
-		//db.getTeam()
+		//a garder dans l'interface graphique ==> classement : boutons pour choisir le paramètre pour ordoner
+			//db.getClassement();
+				//boolean team : true=classement/équipe false=classement/joueur
+				//int choice :  0=pets 1=money 2=artefactmoney 3=wave
 		
-		//a faire pour créer un joueur dans une nouvelle équipe
+		db.gameConnection("MatthFlash", "abc123", "Var Motiv = 0");
+		
+		//db.updateTeam("Var Motiv = 0");
+		
+		
+		//a faire pour créer un joueur dans une nouvelle équipe createTeamPlayer ==> appelé dans gameConnection() SI getTeam = false
 		//db.insertTeam(team);
 		//db.insertPlayer(player, password, team);
 		//db.insertHero(player);
@@ -319,20 +471,20 @@ public class database {
 		//db.insertPets(player);
 		//db.updateTeam(team);
 		
-		//a faire pour créer un joueur dans une équipe existante
+		//a faire pour créer un joueur dans une équipe existante createPlayer() ==> appelé dans gameConnection SI getTeam() = true
 		//db.insertPlayer(player, password, team);
 		//db.insertHero(player);
 		//db.insertMonster(player);
 		//db.insertPets(player);
 		//db.updateTeam(team);
 		
-		//a faire lors de la connexion
+		//a faire lors de la connexion : gameConnection
 		//db.getPlayer()
 		//db.getHero()
 		//db.getMonster()
 		//db.getPets()
 		
-		//a faire lors de la deconnexion
+		//a faire lors de la deconnexion : gameDeconnection()
 		//db.updateHero(player);
 		//db.updateMonster(player);
 		//db.updatePets(player);
